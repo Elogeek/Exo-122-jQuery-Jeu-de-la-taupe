be7628fbabe5a53a70ffa91bbbcb0d23ab479526
Elogeek <elodiechristin@gmail.com>
2021-01-12 15:13:10 +0100

    go mole

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const holes = $(".mole");
+const scoreBoard = $(".score");
+
+let score = 0;
+
+function goGame() {}
